afe85577b58503e0a53c295cac36a2e431531edb
henilcodes <prajapatihenil1112@gmail.com>
2022-11-14 13:56:36 +0530

    edit

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,3 @@
 <?php
-// $conn = mysqli_connect("sql300.epizy.com", "epiz_32115700", "ykadE1TpC3","epiz_32115700_Chat");
-
-$conn = mysqli_connect("localhost", "root", "", "live_chat");
+$conn = mysqli_connect("localhost", "root", "", "web_chat");
 ?>
